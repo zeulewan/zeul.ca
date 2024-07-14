9f9b2224601365e1d1aca5ca3ee47570af7ed2f8
zeulewan <zeul@mordasiewicz.com>
2024-07-14 16:44:28 +0100

    update blogpermissions.sh

--- blogpermissions.sh
+++ blogpermissions.sh
@@ -5,9 +5,9 @@ BLOG_DIR="blog"
 
 
 # Set the ownership to www-data for the blog directory and all its contents
-chown -R www-data:www-data "$BLOG_DIR"
+chown -R zeul:www-data "$BLOG_DIR"
 
 # Set the permissions to rwxrwx--- for the blog directory and all its contents
-chmod -R 770 "$BLOG_DIR"
+chmod -R 760 "$BLOG_DIR"
 
 echo "The blog directory has been set to rwxrwx--- with www-data as the owner and group."
